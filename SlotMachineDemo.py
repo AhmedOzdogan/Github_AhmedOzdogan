ef59999a421ddc45fae4469cecc15710557e0a1b
Ahmed Özdoğan <ahmeddozdogan@gmail.com>
2022-08-07 19:54:22 +0700

    updates abd bug fixes

--- SlotMachineDemo.py
+++ SlotMachineDemo.py
@@ -5,7 +5,6 @@ import random
 
 class SlotMachine():
     
-    items = []
     firstLine = []
     secondLine = []
     thirdLine = []
@@ -20,21 +19,23 @@ class SlotMachine():
         self.item_4 = item_4
         self.item_5 = item_5
 
-        self.items.append(self)
-        
     def cont (self) :
-        
-        a = input("Do you want to continue? y/n")
-        
-        if a.lower() in ["y","yes"]:
-            self.firstLine.clear()
-            self.secondLine.clear()
-            self.thirdLine.clear()
-        else:
-            print("*** Thanks for Playing***")
-            print("Final is " + str(self.cash))
+         if self.cash == 0:
+             print("You lost!!!\n*** Thanks for Playing***")
+             
+         else:
+            a = input("Do you want to continue? y/n")
             
-            self.start()            
+            if a.lower() in ["y","yes"]:
+                self.firstLine.clear()
+                self.secondLine.clear()
+                self.thirdLine.clear()
+                self.start()           
+            else:
+                print("*** Thanks for Playing***")
+                print("Final is " + str(self.cash))
+            
+             
 
     def fill(self,list1):
         """with this function, we fill the lines. We use random to generate different numbers and 
@@ -42,7 +43,8 @@ class SlotMachine():
         for x in range(3):
             randomNum = random.randint(0, 4)
             list1.append(list(self.__dict__.values())[randomNum])
-    
+            
+
     def result(self,win):
         
         if win > 0:
@@ -62,33 +64,46 @@ class SlotMachine():
         self.cont()
     
     def checker(self):
+        """we will check whether you win or lose by counting the three objects in every single line"""
         win = 0
         fulllist = []
+        results1 = []
+        results2 = []
+        results3 = []
+        
+        for x in (self.firstLine,self.secondLine,self.thirdLine):
+            results1.extend(x)
         
         fulllist.extend(self.firstLine)
         fulllist.extend(self.secondLine)
         fulllist.extend(self.thirdLine)
 
-        if fulllist.count(list(self.__dict__.values())[-1]) >2:
-            win = self.bet * fulllist.count(list(self.__dict__.values())[-1])
-            print("Bonus win is " + str(self.bet * fulllist.count(list(self.__dict__.values())[-1])))
+        if fulllist.count(list(self.__dict__.values())[4]) >3:
+            win = self.bet * fulllist.count(self.item_5)
+            print("Bonus win is " + str(self.bet * fulllist.count(self.item_5) + " with " + self.item_5))
             
+        for x in(0,3,6):
+            for y in (1,4,7):
+                for z in (2,5,8):
+                    c = []
+                    
+                    c.append(results1[x])
+                    c.append(results1[y])
+                    c.append(results1[z])
+                    results2.append(set(c))
+                    
+        for i in results2:
+            if len(i)== 1 and i:
+                results3.append(i)
 
-        for x in (self.firstLine[0],self.secondLine[0],self.thirdLine[0]):
-            index = 1
-
-            if x == self.firstLine[index] or x == self.secondLine[index] or x == self.thirdLine[index]:
-                index +=1
-                
-                if x == self.firstLine[index] or x == self.secondLine[index] or x == self.thirdLine[index]:
-                    if x in (list(self.__dict__.values())[:2]):
-                        win = win + self.bet*2
-                        print( x + " line win is " + str(self.bet*2))
-                    else:
-                        win = win + self.bet*4
-                        print( x + " line win is " + str(self.bet*4))
-                        
-
+        
+        for x in results3:
+            if list(x)[0] in list(self.__dict__.values())[3:5] :
+                win = win + self.bet*2
+                print("You win " + str(self.bet*2) + " with " + str(list(x)[0]) +" line!!!")
+            else:
+                win = win + self.bet
+                print("You win " + str(self.bet) + " with " + str(list(x)[0]) +" line!!!")
         if win == 0 :
             self.cash = self.cash - self.bet
 
@@ -106,12 +121,19 @@ class SlotMachine():
         self.fill(self.thirdLine)
         
         self.checker()
-        
-
 
 a = SlotMachine("J","Q","K","A","Bonus")
+b = SlotMachine("Apple","Orange","Cherry","Kiwi","Wild")
 
-a.start()
+print("Welcome to the Game \nGood Luck!")
+choice = input("we have two sets availbale \nyou can either play with cards or fruits.\n(1 for deck, 2 for fruits)")
+
+if choice == "1":
+    a.start()
+elif choice == "2":
+    b.start()
+else:
+    print("invalid input!!!\nPlease try again later!")
 
 
 
